f9e2e4b75fa595dba7eccd8e68b29c63b5cbff9f
Rai <nurairstalk@proton.me>
2026-02-11 17:30:54 +0700

    fix footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Footer() {
     return (
-        <div className="py-10 border-t-5 border-shelbypink bg-chocodark text-center mt-50">
+        <div className="py-10 border-t-5 border-shelbypink bg-chocodark text-center">
             <p className="credit text-white">Created with 🩷 by <a className="text-blue-200 font-bold" href="https://x.com/nurairstalk">Rai</a></p>
         </div>
     )
